5410fdd4f6058310571811e75a2dc6e2ec86fa45
LEEHYEYEONG <elto789@naver.com>
2022-05-16 13:48:22 +0900

    #4.10 react query

--- reactjs_master/src/api.ts
+++ reactjs_master/src/api.ts
@@ -1,7 +1,16 @@
 import { json } from "stream/consumers";
 
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
 export async function fetchCoins() {
-  return fetch("https://api.coinpaprika.com/v1/coins")
-  .then((response) => response.json());
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
   
+}
+
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) => response.json());
+}
+
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
 }
