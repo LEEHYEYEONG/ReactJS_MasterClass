27c1717c6e42259482254b874d1744acb0835ba1
LEEHYEYEONG <elto789@naver.com>
2022-07-20 00:42:19 +0900

    #5.7

--- reactjs_master/src/ToDoList.tsx
+++ reactjs_master/src/ToDoList.tsx
@@ -30,15 +30,30 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
-	return (
-		<div>
-      <form>
-        <input {...register("toDo")} placeholder="Write a to do" />
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data)
+  };
+  console.log(formState.errors);
+
+  return (
+    <div>
+      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
+        <input {...register("email", { required: "email is required" })} placeholder="Write a to email" />
+        <input {...register("firstName", { required: true })} placeholder="Write a to firstname" />
+        <input {...register("lastName", { required: true })} placeholder="Write a to lastname" />
+        <input {...register("username", { required: true, minLength: {
+          value: 10,
+          message: "username too short"
+        }
+         })} placeholder="Write a to username" />
+        <input {...register("password", { required: true, minLength: 5 })} placeholder="Write a to password" />
+        <input {...register("password1", { required: true, minLength: 5 })} placeholder="Write a to password1" />
+
         <button>Add</button>
       </form>
     </div>
-	)
+  )
 };
 
 export default ToDoList;
